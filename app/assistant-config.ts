613ae330b9ebae9295712e57df810b65d065d282
willis Chen <willismax.com@gmail.com>
2024-06-07 08:20:17 +0800

    Update assistant-config.ts

--- app/assistant-config.ts
+++ app/assistant-config.ts
@@ -1,5 +1,5 @@
 export let assistantId = ""; // set your assistant ID here
 
-if (assistantId === "") {
+if (assistantId === "asst_FZCDq0xR0Hqt9QGn4tH7IthG") {
   assistantId = process.env.OPENAI_ASSISTANT_ID;
 }
